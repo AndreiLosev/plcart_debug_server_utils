c31c55eb5b8628bee41ce72c4270e25374df6e36
andrei <loseu.andre.g@gmail.com>
2024-06-09 08:37:12 +0300

    add set debug value

--- lib/src/client_command.dart
+++ lib/src/client_command.dart
@@ -7,7 +7,8 @@ enum CommandKind {
   getRegisteredTasks,
   runEvent,
   subscribeTask,
-  unsubscribeTask;
+  unsubscribeTask,
+  setTaskValue;
 
   int code() => switch (this) {
         CommandKind.getRegisteredEvents => 3,
@@ -15,6 +16,7 @@ enum CommandKind {
         CommandKind.runEvent => 10,
         CommandKind.subscribeTask => 20,
         CommandKind.unsubscribeTask => 30,
+        CommandKind.setTaskValue => 40,
       };
 }
 
@@ -25,6 +27,7 @@ extension ToCommandKind on int {
         10 => CommandKind.runEvent,
         20 => CommandKind.subscribeTask,
         30 => CommandKind.unsubscribeTask,
+        40 => CommandKind.setTaskValue,
         _ => throw Exception("invalide CommandKind code: $this"),
       };
 }
@@ -51,14 +54,35 @@ class RunEventPayload {
   }
 }
 
+class SetTaskValuePayload {
+  final Object value;
+  final int? index;
+  final String? sIndex;
+  final ActionValuePayload? action;
+
+  SetTaskValuePayload(Map map)
+      : value = map['value'],
+        index = map['index'],
+        sIndex = map['sIndex'],
+        action = map['action'];
+}
+
+enum ActionValuePayload {
+  add,
+  remove;
+}
+
 ClientCommand parseClientCommand(Uint8List bytes) {
   final map = deserialize(bytes) as Map;
   final kind = (map['CommandKind'] as int).toCommandKind();
   return switch (kind) {
     CommandKind.getRegisteredEvents => ClientCommand(kind, null),
     CommandKind.getRegisteredTasks => ClientCommand(kind, null),
-    CommandKind.runEvent => ClientCommand(kind, RunEventPayload(map['payload'])),
+    CommandKind.runEvent =>
+      ClientCommand(kind, RunEventPayload(map['payload'])),
     CommandKind.subscribeTask => ClientCommand(kind, map['payload'] as String),
-    CommandKind.unsubscribeTask => ClientCommand(kind, map['payload'] as String),
+    CommandKind.unsubscribeTask =>
+      ClientCommand(kind, map['payload'] as String),
+    CommandKind.setTaskValue => ClientCommand(kind, map['payload']),
   };
 }
